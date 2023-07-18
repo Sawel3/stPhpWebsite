02d6252b5b8adcc8f1779c7fd682d3e6467978de
Sawel3 <pawciosa@gmail.com>
2023-07-18 18:39:53 +0200

    Added most of the functions

--- index.php
+++ index.php
@@ -1,22 +1,86 @@
 <?php
-$page = $_SERVER['PHP_SELF'];
-$sec="15";
-?>
+    include("database_connect.php");
 
+    if(mysqli_connect_errno()){
+        echo"Failed to connect to MySQL: ". mysqli_connect_errno();
+    }
+    $result = mysqli_query($con,"SELECT *FROM ESPtable2");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Arduino controler</title>
+    <meta http-equiv="refresh" name="viewport" content="width=device-width, initial-scale=1.0" content="<?php echo $sec?>; URL='<?php echo $page?>'">
+    <title>Moja strona</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="title">
-        <h1>Arduino controller</h1>
-    </div>
+    <table action="<?php $_SERVER["PHP_SELF"]?>" class='table' style='font-size:3vw;'>
+        <thead>
+            <tr>
+                <th>Boolean indicator</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class='active'>
+                <td>Sawel ID</td>
+                <td>Boolean control 1</td>
+            </tr>
+            <?php
+            while($row=mysqli_fetch_array($result)){
+            echo "<tr class='success'>";
+            $unit_id=$row['id'];
+            echo "<td>" .$row['id'] ."</td>";
+
+            $column1 = "RECEIVED_BOOL1";
+
+            $current_bool_1=$row['RECEIVED_BOOL1'];
+
+            if($current_bool_1==1){
+                $inv_current_bool_1=0;
+                $text_current_bool_1="ON";
+                $color_current_bool_1="#6ed829";
+            }
+            else{
+                $inv_current_bool_1=1;
+                $text_current_bool_1="OFF";
+                $color_current_bool_1="#e04141";
+            }
+            
+            echo "<td>
+            <form action=update_values.php method='post'>
+            <input type='hidden' name='value2' value=$current_bool_1 >
+            <input type='hidden' name='value' value= $inv_current_bool_1 >
+            <input type='hidden' name='unit' value=$unit_id>
+            <input type='hidden' name='vcolumn' value=$column1>
+            <input type='submit' name='change_but' style='margin-left:25%; margin-top:10%; font-size:1.5vw; text-align:center; background-color:$color_current_bool_1' value= $text_current_bool1>
+            </form>
+            </td>";
+            }
+            ?>
+        </tr>
+        </tbody>
+    </table> <br>
 </body>
 </html>
+
 <?php
-echo"siema";
+    while($row=mysqli_fetch_array($result)){
+        $unit_id=$row['id'];
+        
+        $column1 = "RECEIVED_BOOL1";
+
+        $current_bool_1=$row['RECEIVED_BOOL1'];
+        
+        if($current_bool_1==1){
+            $inv_current_bool_1=0;
+            $text_current_bool_1="ON";
+            $color_current_bool_1="#6ed829";
+        }
+        else{
+            $inv_current_bool_1=1;
+            $text_current_bool_1="OFF";
+            $color_current_bool_1="#e04141";
+        }
+    }
 ?>
---
+++ update_values.php
@@ -0,0 +1,12 @@
+<?php
+$value = $_POST['value'];
+$unit = $_POST['unit'];
+$column = $_POST['column'];
+
+include("database_connect.php");
+if(mysqli_connect_errno()){
+    echo"Failed to connect to MySQL: ". mysqli_connect_errno();
+ }
+ mysqli_query($con,"UPDATE ESPtable2 SET $column = '{$value}' WHERE id=$unit");
+ header("location: index.php");
+?>
