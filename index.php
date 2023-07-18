06bdfd7da3e7234df96f8b50e680e5615ece0475
Sawel3 <pawciosa@gmail.com>
2023-07-18 13:31:55 +0200

    Added basic

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$page = $_SERVER['PHP_SELF'];
+$sec="15";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arduino controler</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="title">
+        <h1>Arduino controller</h1>
+    </div>
+</body>
+</html>
+<?php
+echo"siema";
+?>
